c575fa497b064256216fc8a5392c9b1ed690b1b8
Ben Small <bsmall@wayfair.com>
2019-07-09 15:29:24 -0400

    Bug Fix for Get-VaultWrapping
    Increments vaultUtils to 1.1.1.
    Fixes a bug/feature tweak where if the token being specified is a vault-wrapped token, specify that token in the HEADER and do not require a VAULT_TOKEN to retrieve the wrapped token.

--- vaultUtils/Public/Wrapping/Get-VaultWrapping.ps1
+++ vaultUtils/Public/Wrapping/Get-VaultWrapping.ps1
@@ -1,10 +1,10 @@
 function Get-VaultWrapping {
 <#
 .Synopsis
-    Retrieves a wrapped key-value pair, or series of key-value pairs, given a specified token.
+    Retrieves a wrapped key-value pair, series of key-value pairs or vault-wrapped token, given a specified token.
 
 .DESCRIPTION
-    Get-VaultWrapping retrieves a wrapped key-value pair or collection of key-value pairs, given a specified wrapping token.
+    Get-VaultWrapping retrieves a wrapped key-value pair, collection of key-value pairs, or a vault-wrapped token, given a specified wrapping token.
 
 .EXAMPLE
     PS> Get-VaultWrapping -Token s.1G4mBRbpvZ7TQEj0Cf90MImO
@@ -17,6 +17,23 @@ function Get-VaultWrapping {
     wrap_info      :
     warnings       :
     auth           :
+
+.EXAMPLE
+    PS> Get-VaultWrapping -Token s.2Dx7RLfOFg4E90p1oFeQR4C1 -IsWrappingToken
+
+    request_id     : 6f2cf0ab-0a8e-d475-c24a-bea085cb1d2a
+    lease_id       :
+    renewable      : False
+    lease_duration : 0
+    data           :
+    wrap_info      :
+    warnings       :
+    auth           : @{client_token=s.NVKrGA1rzMoY9UZD2Ca50BOx; accessor=X1WvPXClCeMUPR1S8uxhZGRe; policies=System.Object[];
+                    token_policies=System.Object[]; identity_policies=System.Object[]; metadata=; lease_duration=129600;
+                    renewable=True; entity_id=357f788d-75cf-c16d-f6d9-cdbd6c5deee8; token_type=service; orphan=False}
+
+    This example demonstrates the retrieval of a vault-wrapped token. 
+    Notably (but not shown) a VAULT_TOKEN does not need to be set to retrive a vault-wrapped token.
 #>
     [CmdletBinding()]
     param(
@@ -26,38 +43,61 @@ function Get-VaultWrapping {
         )]
         [String] $Token,
 
-        #Specifies how output information should be displayed in the console. Available options are JSON or PSObject.
+        #Specifies that the token being specified is an unwrapping token.
         [Parameter(
             Position = 1
+        )]
+        [Switch] $IsWrappingToken,
+
+        #Specifies how output information should be displayed in the console. Available options are JSON or PSObject.
+        [Parameter(
+            Position = 2
         )]
         [ValidateSet('Json','PSObject')]
         [String] $OutputType = 'PSObject',
 
         #Specifies whether or not just the data should be displayed in the console.
         [Parameter(
-            Position = 2
+            Position = 3
         )]
         [Switch] $JustData
     )
 
     begin {
-        Test-VaultSessionVariable -CheckFor 'Address','Token','Cred','LoginMethod'
+        #If the token being specified is a wrapping token, we only need an address.
+        if ($IsWrappingToken) {
+            Test-VaultSessionVariable -CheckFor 'Address'
+        }
+        else {
+            Test-VaultSessionVariable -CheckFor 'Address','Token','Cred','LoginMethod'
+        }
     }
 
     process {
+        $uri = $global:VAULT_ADDR
+
         $jsonPayload = @"
 {
     "token": "$Token"
 }
 "@
 
-        $uri = $global:VAULT_ADDR
-
         $irmParams = @{
             Uri    = "$uri/v1/sys/wrapping/unwrap"
-            Header = @{ "X-Vault-Token" = $global:VAULT_TOKEN }
             Method = 'Post'
-            Body   = $($jsonPayload | ConvertFrom-Json | ConvertTo-Json -Compress)
+        }
+
+
+        if ($IsWrappingToken) {
+            $irmParams += @{ 
+                Header = @{ "X-Vault-Token" = $Token }
+            }
+        }
+        else {
+            $irmParams += @{ 
+                Header = @{ "X-Vault-Token" = $global:VAULT_TOKEN }
+                Body   = $($jsonPayload | ConvertFrom-Json | ConvertTo-Json -Compress)
+            }
         }
 
         try {
